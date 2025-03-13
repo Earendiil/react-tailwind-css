d0b593f934dfcb66af308961e946539c9f24f3ba
teo <teo.th@hotmail.gr>
2025-03-13 11:27:44 +0100

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-6">
-      <h1 className="text-4xl font-bold">Hello, Tailwind!</h1>
+    <div className="bg-gray-100 p-4">
+      <ul className='list-none md:flex md:gap-4'>
+        <li className='bg-blue-500 text-white p-2 m-2 rounded-lg md:px-4'>Item 1</li>
+        <li className='bg-blue-500 text-white p-2 m-2 rounded-lg md:px-4'>Item 2</li>
+        <li className='bg-blue-500 text-white p-2 m-2 rounded-lg md:px-4'>Item 3</li>
+      </ul>
     </div>
   );
 }
